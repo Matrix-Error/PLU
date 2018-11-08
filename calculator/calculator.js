ea341d05f177f44b5bd715b78b0a3f08827ab840
unknown <sarabiasoliveira@gmail.com>
2018-11-08 16:26:24 +0000

    experiencias a criar a calculadora

---
+++ calculator/calculator.js
@@ -0,0 +1,37 @@
+let n1,n2
+
+function sum() {
+    
+    
+    n1=Number(document.querySelector("#num1").value)
+    n2=Number(document.querySelector("#num2").value)
+
+    alert(n1+n2)
+}
+
+function divide() {
+    let n1,n2
+    
+    n1=Number(document.querySelector("#num1").value)
+    n2=Number(document.querySelector("#num2").value)
+
+    alert(n1/n2)
+}
+
+function multi() {
+    let n1,n2
+    
+    n1=Number(document.querySelector("#num1").value)
+    n2=Number(document.querySelector("#num2").value)
+
+    alert(n1*n2)
+}
+
+function sub() {
+    let n1,n2
+    
+    n1=Number(document.querySelector("#num1").value)
+    n2=Number(document.querySelector("#num2").value)
+
+    alert(n1-n2)
+}
